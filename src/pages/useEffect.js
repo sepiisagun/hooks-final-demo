72499f29a62769e681aed8799ecfa55147a91a7e
Jimmy II Caticat <20161084@s.ubaguio.edu>
2023-06-20 23:55:57 +0800

    added stacked bar on top of progress bar of useEffect to signify changing of color

--- src/pages/useEffect.js
+++ src/pages/useEffect.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useReducer, useState } from 'react';
 
 function Reducer(state, action) {
+	// this is for incrementing the progress bar
 	switch (action) {
 		case 'plus':
 			let maxWidth = state.width;
 			if (maxWidth < 1300) {
-				maxWidth += 130;
+				maxWidth += 162.5;
 			}
 			return { width: maxWidth };
 		default:
@@ -18,11 +19,11 @@ function UseEffect() {
 	const [color, setColor] = useState([]);
 
 	useEffect(() => {
-		if (state.width === 390) {
+		if (state.width === 325) {
 			setColor([...color, 'btn-secondary']);
 		} else if (state.width === 650) {
 			setColor([...color, 'btn-primary']);
-		} else if (state.width === 1040) {
+		} else if (state.width === 975) {
 			setColor([...color, 'btn-success']);
 		} else if (state.width === 1300) {
 			setColor([...color, 'btn-danger']);
@@ -30,6 +31,52 @@ function UseEffect() {
 	}, [state]); // based on the state, the side effect is that the circles should change colors depending on the current state value
 	return (
 		<div className="container mt-5">
+			<div class="progress-stacked br">
+				<div
+					class="progress"
+					role="progressbar"
+					aria-label="Segment one"
+					aria-valuenow="15"
+					aria-valuemin="0"
+					aria-valuemax="100"
+					style={{ width: '390px' }}
+				>
+					<div class="progress-bar bg-secondary"></div>
+				</div>
+				<div
+					class="progress"
+					role="progressbar"
+					aria-label="Segment two"
+					aria-valuenow="30"
+					aria-valuemin="0"
+					aria-valuemax="100"
+					style={{ width: '390px' }}
+				>
+					<div class="progress-bar bg-primary"></div>
+				</div>
+				<div
+					class="progress"
+					role="progressbar"
+					aria-label="Segment three"
+					aria-valuenow="20"
+					aria-valuemin="0"
+					aria-valuemax="100"
+					style={{ width: '390px' }}
+				>
+					<div class="progress-bar bg-success"></div>
+				</div>
+				<div
+					class="progress"
+					role="progressbar"
+					aria-label="Segment three"
+					aria-valuenow="20"
+					aria-valuemin="0"
+					aria-valuemax="100"
+					style={{ width: '390px' }}
+				>
+					<div class="progress-bar bg-danger"></div>
+				</div>
+			</div>
 			<div
 				className="progress"
 				role="progressbar"
@@ -40,7 +87,7 @@ function UseEffect() {
 				style={{ height: '48px' }}
 			>
 				<div
-					className="progress-bar"
+					className="progress-bar progress-bar-striped progress-bar-animated"
 					style={{ width: state.width }}
 				></div>
 			</div>
